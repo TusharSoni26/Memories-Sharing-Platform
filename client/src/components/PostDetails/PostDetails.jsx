78ad251d7ae1ecaf3054f0ee825c27c04fc9d2e8
Tushar Soni <tusharsachin1soni@gmail.com>
2022-09-11 16:38:25 +0530

    Search and Paination added

---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostDetails = () => {
+    console.log('Post Detail');
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostDetails
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,7 +11,10 @@ export const signin = async (req, res) => {
     try {
         const oldUser = await UserModal.findOne({ email });
 
-        if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+        if (!oldUser)
+        {
+            return res.status(404).json({ message: "User doesn't exist" });
+        }
 
         const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
@@ -31,7 +34,9 @@ export const signup = async (req, res) => {
     try {
         const oldUser = await UserModal.findOne({ email });
 
-        if (oldUser) return res.status(400).json({ message: "User already exists" });
+        if (oldUser){
+            return res.status(400).json({ message: "User already exists" });
+        }
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
